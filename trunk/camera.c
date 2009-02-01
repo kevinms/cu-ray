b345e5b5929be1f1979e6b637d9c8989ec3fd492
kevin4@g.clemson.edu <kevin4@g.clemson.edu@da11cbfa-f01e-11dd-8c4e-4bbb75a408b7>
2009-02-01 05:15:58 +0000

    Initial commit

---
+++ trunk/camera.c
@@ -0,0 +1,140 @@
+/* camera.c */
+
+/* This module contains the functions that involve the camera */
+
+#include "ray.h"
+
+static char *cam_attrs[] = { "pixeldim", "worlddim", "viewpoint" };
+#define NUM_ATTRS (sizeof(cam_attrs)/sizeof(char *))
+
+static inline void cam_load_pixeldim(FILE *in, cam_t *cam)
+{
+   int count;
+   assert(cam->cookie == CAM_COOKIE);
+   count = fscanf(in, "%d %d",
+                  &cam->pixel_dim[0], &cam->pixel_dim[1]);
+/* ensure that the required number of values were found */
+   assert(count == 2);
+}
+
+static inline void cam_load_worlddim(FILE *in, cam_t *cam)
+{
+   int count;
+   assert(cam->cookie == CAM_COOKIE);
+   count = fscanf(in, "%lf %lf",
+                  &cam->world_dim[0], &cam->world_dim[1]);
+
+/* ensure that the required number of values were found */
+   assert(count == 2);
+}
+
+static inline void cam_load_viewpoint(FILE *in, cam_t *cam)
+{
+   int count;
+   assert(cam->cookie == CAM_COOKIE);
+   count = fscanf(in, "%lf %lf %lf",
+                  &cam->view_point.x, &cam->view_point.y,
+                  &cam->view_point.z);
+/* ensure that the required number of values were found */
+   assert(count == 3);
+}
+
+static inline void cam_attr_load(FILE *in, cam_t *cam, char *attr)
+{
+   int ndx;
+
+/* verify structure pointer */
+   assert(cam->cookie == CAM_COOKIE);
+
+/* Perform table lookup and ensure it worked */
+   ndx = table_index(cam_attrs, NUM_ATTRS, attr);
+   assert(ndx >= 0);
+
+/* Remember which attribute was found */
+   cam->attrmask |= 1 << ndx;
+   if (ndx == 0)
+      cam_load_pixeldim(in, cam);
+   else if (ndx == 1)
+      cam_load_worlddim(in, cam);
+   else
+      cam_load_viewpoint(in, cam);
+}
+
+/**/
+/* Initialize camera data */
+
+void camera_init(FILE *in, model_t *model, int attrmax)
+{
+   char  buf[256];
+   int count;
+
+   cam_t *cam  = (cam_t *)malloc(sizeof(cam_t));
+   assert(cam != NULL);
+   memset(cam, 0, sizeof(cam));
+   cam->cookie = CAM_COOKIE;
+
+   assert(fscanf(in, "%s", cam->name) == 1);
+
+   fscanf(in, "%s", buf);
+   assert(buf[0] == '{');
+
+   count = fscanf(in, "%s", buf);
+   while( (count == 1) && (buf[0] != '}') )
+      {
+         cam_attr_load(in, cam, buf);
+         *buf = 0;
+         fscanf(in, "%s", buf);
+      }
+
+   assert(buf[0] == '}');
+   assert(cam->attrmask == CAM_MASK);
+
+   cam->pixmap = malloc(cam->pixel_dim[0] * 
+		cam->pixel_dim[1] * sizeof(irgb_t));
+
+   model->cam = cam;
+}
+
+/**/
+/* Dump the camera data */
+
+void camera_dump(
+FILE   *out,
+cam_t *cam)
+{
+   assert(cam->cookie == CAM_COOKIE);
+
+   fprintf(out, "%-12s %s\n", "camera", cam->name);
+
+   fprintf(out, "%-12s %5d %5d\n", "pixel_dim", 
+		cam->pixel_dim[0], cam->pixel_dim[1]);
+
+   fprintf(out, "%-12s %5.1lf %5.1lf\n", "world_dim", 
+		cam->world_dim[0], cam->world_dim[1]);
+
+   fprintf(out, "%-12s %5.1lf %5.1lf %5.1lf\n", "view_point", 
+		cam->view_point.x, cam->view_point.y, cam->view_point.z);
+}
+
+/**/
+/* Compute the direction of a ray fired through pixel */
+/* coordinates (x, y)                                 */
+
+void camera_getdir(
+cam_t    *cam,
+int      x,
+int      y,
+vec_t   *dir)
+{
+
+   assert(cam->cookie == CAM_COOKIE);
+
+   double world_x = cam->world_dim[0] * x / (cam->pixel_dim[0] - 1);
+   double world_y = cam->world_dim[1] * y / (cam->pixel_dim[1] - 1);
+
+   vec_t pix = {world_x, world_y, 0.0};
+
+   vec_diff(&cam->view_point, &pix, dir);
+
+   vec_unit(dir, dir);
+}
---
+++ trunk/material.c
@@ -0,0 +1,149 @@
+#include "ray.h"
+
+static char *mat_attrs[] = { "ambient", "diffuse", "specular" };
+#define NUM_ATTRS (sizeof(mat_attrs) / sizeof(char *))
+
+static inline void mat_load_ambient(FILE *in, material_t *mat)
+{
+   int count;
+   assert(mat->cookie == MAT_COOKIE);
+   count = fscanf(in, "%lf %lf %lf",
+                  &mat->ambient.r, &mat->ambient.g,
+                  &mat->ambient.b);
+/* ensure that the required number of values were found */
+   assert(count == 3);
+}
+
+static inline void mat_load_diffuse(FILE *in, material_t *mat)
+{
+   int count;
+   assert(mat->cookie == MAT_COOKIE);
+   count = fscanf(in, "%lf %lf %lf",
+                  &mat->diffuse.r, &mat->diffuse.g,
+                  &mat->diffuse.b);
+/* ensure that the required number of values were found */
+   assert(count == 3);
+}
+
+static inline void mat_load_specular(FILE *in, material_t *mat)
+{
+   int count;
+   assert(mat->cookie == MAT_COOKIE);
+   count = fscanf(in, "%lf %lf %lf",
+                  &mat->specular.r, &mat->specular.g,
+                  &mat->specular.b);
+/* ensure that the required number of values were found */
+   assert(count == 3);
+}
+
+static int material_attr_load(
+FILE      *in,
+material_t *mat,       /* material to be filled in */
+char      *attrname)
+{
+	int ndx;
+
+	assert(mat->cookie = MAT_COOKIE);
+
+	ndx = table_lookup(mat_attrs, NUM_ATTRS, attrname);
+	assert(ndx >= 0);
+	switch(ndx)
+	{
+		case 0:
+			mat_load_ambient(in, mat); return 0;
+		case 1:
+			mat_load_diffuse(in, mat); return 0;
+		case 2:
+			mat_load_specular(in, mat); return 0;
+		default:
+			return -1;
+	}
+}
+
+/* Create a new material description */
+void material_init(
+FILE       *in,
+model_t    *model,
+int        attrmax)
+{
+	material_t *mat;
+	char attrname[NAME_LEN];
+	int count;
+	
+	mat = (material_t *)malloc(sizeof(material_t));
+	assert(mat != NULL);
+	memset(mat, 0, sizeof(mat));
+	mat->cookie = MAT_COOKIE;
+
+	assert(fscanf(in, "%s", mat->name) == 1);
+
+	fscanf(in, "%s", attrname);
+	assert(attrname[0] == '{');
+
+	count = fscanf(in, "%s", attrname);
+	while( (count == 1) && (attrname[0] != '}') )
+	{
+		assert(material_attr_load(in, mat, attrname) == 0);
+		*attrname = 0;
+		fscanf(in, "%s", attrname);
+	}
+
+	assert(attrname[0] == '}');
+
+	list_add(model->mats, mat);
+}
+
+
+/* Produce a formatted dump of the material list */
+void material_dump(
+FILE *out,
+model_t *model)
+{
+	link_t *templink;
+	templink = model->mats->head;
+	material_t *tempmat;
+	while(templink != NULL)
+	{
+		tempmat = (material_t *)templink->item;
+		fprintf(stderr, "material     %s \n", tempmat->name);
+		fprintf(stderr, "ambient %10.1lf %5.1lf %5.1lf \n",
+                        tempmat->ambient.r, 
+						tempmat->ambient.g, 
+						tempmat->ambient.b);
+		fprintf(stderr, "diffuse %10.1lf %5.1lf %5.1lf \n",
+                        tempmat->diffuse.r, 
+						tempmat->diffuse.g, 
+						tempmat->diffuse.b);
+		fprintf(stderr, "specular %9.1lf %5.1lf %5.1lf \n\n",
+                        tempmat->specular.r, 
+						tempmat->specular.g, 
+						tempmat->specular.b);
+		
+		templink = templink->next;
+	}
+}
+
+
+/* Search the material list looking for a material */
+/* having the specified color name. If found,      */
+/* return the address of the material_t structure  */
+/* If not, found return NULL                        */
+
+material_t *material_search(
+model_t *model,
+char    *name)     // e.g. orange
+{
+	link_t *templink;
+	material_t *tempmat;
+	templink = model->mats->head;
+	while(templink->next != NULL)
+	{
+		tempmat = (material_t *)templink->item;
+		if( strcmp(tempmat->name, name) == 0 )
+			return tempmat;
+		
+
+		templink = templink->next;
+	}
+	return NULL;
+}
