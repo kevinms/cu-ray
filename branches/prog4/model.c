04db8864a860ffb15f9ed9967bef4a13132abe9c
kevin4@g.clemson.edu <kevin4@g.clemson.edu@da11cbfa-f01e-11dd-8c4e-4bbb75a408b7>
2009-02-19 21:53:02 +0000

    A couple more files

---
+++ branches/prog4/model.c
@@ -0,0 +1,116 @@
+/**********************************************************************
+ * Kevin Matthew Smith && Burns John Hudson
+ * kevin4 && burnsh
+ * CpSc 102 Section 003
+ * February 9, 2009
+ * Program 3
+ *
+ * Description
+ * This program alocates memory for a model structure and parses and
+ * loads the data for the structure.  Also a model_dump() function.
+ ***********************************************************************/
+
+
+/* model.c */
+
+#include "ray.h"
+
+static char *entities[] =
+{
+    "camera",
+    "material",
+	"sphere",
+	"plane",
+};
+
+#define NUM_ITEMS (sizeof(entities) / sizeof(entities[0]))
+
+static inline void model_item_load(
+FILE    *in,
+model_t *model,
+char    *enttype)
+{
+   int ndx;
+
+   ndx = table_lookup(entities, NUM_ITEMS, enttype);
+   assert(ndx >= 0);
+
+   if (ndx == 0)
+   {
+      camera_init(in, model, 0);
+   }
+   else if (ndx == 1)
+   {
+      material_init(in, model, 0);
+   }
+   else if (ndx == 2)
+   {
+      sphere_init(in, model, 0);
+   }
+   else if (ndx == 3)
+   {
+      plane_init(in, model, 0);
+   }
+
+   return;
+}
+
+/* Load model data */
+
+static void model_load(
+FILE    *in,
+model_t *model)
+{
+   char enttype[16];
+   int  count;
+
+   memset(enttype, 0, sizeof(enttype));
+
+/* Here enttype should be one of "material",    */
+/* "light", "plane"                             */
+
+   count = fscanf(in, "%s", enttype);
+   while (count == 1)
+   {
+      model_item_load(in, model, enttype);
+      count = fscanf(in, "%s", enttype);
+   }
+}
+
+/**/
+/* Init model data */
+
+model_t *model_init(
+FILE *in)
+{
+   model_t *model = malloc(sizeof(model_t));
+   assert(model != NULL);
+
+   model->mats = list_init();
+   assert(model->mats != NULL);
+
+   model->lgts = list_init();
+   assert(model->lgts != NULL);
+
+   model->objs = list_init();
+   assert(model->objs != NULL);
+
+   model_load(in, model);
+
+   return(model);
+
+}
+
+/**/
+/* dump model data */
+
+void model_dump(
+FILE    *out,
+model_t *model)
+{
+   camera_dump(out, model->cam);
+   material_dump(out, model);
+   object_dump(out, model);
+// light_dump(out, model);
+}
+
---
+++ branches/prog4/plane.c
@@ -0,0 +1,104 @@
+/**********************************************************************
+ * Kevin Matthew Smith && Burns John Hudson
+ * kevin4 && burnsh
+ * CpSc 102 Section 003
+ * February 9, 2009
+ * Program 3
+ *
+ * Description
+ * This program alocates memory for a plane structure and parses and
+ * loads the data for the structure.  Also a plane_dump() function.
+ ***********************************************************************/
+
+#include "ray.h"
+
+static char *pln_attrs[] = { "point", "normal" };
+#define NUM_ATTRS (sizeof(pln_attrs) / sizeof(char *))
+
+static inline void pln_load_point(FILE *in, plane_t *pln)
+{
+   int count;
+   count = fscanf(in, "%lf %lf %lf", &pln->point.x, 
+					&pln->point.y, &pln->point.z);
+/* ensure that the required number of values were found */
+   assert(count == 3);
+}
+
+static inline void pln_load_normal(FILE *in, plane_t *pln)
+{
+   int count;
+   count = fscanf(in, "%lf %lf %lf", &pln->normal.x, 
+					&pln->normal.y, &pln->normal.z);
+/* ensure that the required number of values were found */
+   assert(count == 3);
+}
+
+static int plane_attr_load(
+FILE      *in,
+plane_t *pln,       /* material to be filled in */
+char      *attrname)
+{
+	int ndx;
+
+	ndx = table_lookup(pln_attrs, NUM_ATTRS, attrname);
+	assert(ndx >= 0);
+	switch(ndx)
+	{
+		case 0:
+			pln_load_point(in, pln); return 0;
+		case 1:
+			pln_load_normal(in, pln); return 0;
+		default:
+			return -1;
+	}
+}
+
+object_t *plane_init(
+FILE *in,
+model_t *model,
+int attrmax)
+{
+    char attrname[NAME_LEN];
+    int count;
+	
+	plane_t *pln;
+	object_t *obj;
+	obj = object_init(in, model);
+	pln = (plane_t *)malloc(sizeof(plane_t));
+	assert(pln != NULL);
+	memset(pln, 0, sizeof(pln));
+
+	count = fscanf(in, "%s", attrname);
+	assert(count == 1 && attrname[0] != '}');
+
+	while( (count == 1) && (attrname[0] != '}') )
+	{
+		assert(plane_attr_load(in, pln, attrname) == 0);
+		*attrname = 0;
+		fscanf(in, "%s", attrname);
+	}
+	assert(attrname[0] == '}');
+
+	obj->priv = (void *)pln;
+	sprintf(obj->objtype, "plane");
+	obj->dumper = plane_dump;
+	
+	return obj;
+}
+
+void plane_dump(
+FILE *out,
+object_t *obj)
+{
+	plane_t *pln;
+	pln = (plane_t *)obj->priv;
+        fprintf(stderr, "normal %11.1lf %5.1lf %5.1lf \n",
+                        pln->normal.x, 
+                        pln->normal.y, 
+                        pln->normal.z);
+						
+        fprintf(stderr, "point %12.1lf %5.1lf %5.1lf \n\n",
+                        pln->point.x, 
+                        pln->point.y, 
+                        pln->point.z);
+}
