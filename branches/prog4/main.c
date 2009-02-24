bd5e9b6f2b8007bd49787efe5e815744241561bc
kevin4@g.clemson.edu <kevin4@g.clemson.edu@da11cbfa-f01e-11dd-8c4e-4bbb75a408b7>
2009-02-24 01:22:51 +0000

    new makefile and main.c

---
+++ branches/prog4/main.c
@@ -0,0 +1,16 @@
+/* main.c */
+#include "ray.h"
+int main(
+int argc,
+char *argv[])
+{
+   cam_t   *cam;
+   model_t *model;
+/* Load and dump the model */
+   model = model_init(stdin);
+   model_dump(stderr, model);
+/* Raytrace the image */
+   image_create(model);
+   return(0);
+}
+
