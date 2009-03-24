f2ae427bcf0df0a4bdd2b0d7375b1d3d764cdd7b
kevin4@g.clemson.edu <kevin4@g.clemson.edu@da11cbfa-f01e-11dd-8c4e-4bbb75a408b7>
2009-03-24 22:12:08 +0000

    made prog6 branch

---
+++ branches/prog6/list.h
@@ -0,0 +1,43 @@
+/**********************************************************************
+ * Kevin Matthew Smith && Burns John Hudson
+ * kevin4 && burnsh
+ * CpSc 102 Section 003
+ * March 9, 2009
+ * Program 5
+ *
+ * Description
+ * List structures
+ ***********************************************************************/
+
+class link_t
+{
+public:
+   link_t(void);                  // default constructor
+   link_t(void *);                // overloaded constructor
+   ~link_t (void);                // destructor
+   void    set_next(link_t *);    // used in adding new link
+   link_t *get_next(void);        // retrieve the next pointer
+   void   *get_entity(void);      // retrieve entity pointer
+
+private:
+   link_t *next;                  // next link in the list
+   void   *entity;                // entity managed by link
+};
+
+class list_t
+{
+public:
+   list_t(void);                  // constructor
+   ~list_t (void);                // destructor
+   void   add(void *);            // add entity to end of list
+   void   insert(void *);         // insert entity before current
+   void   remove(void );          // remove current entity
+   void   *start(void);           // set current to start of list
+   void   *get_next(void);        // advance to next element in list
+
+private:
+   link_t *first;                 // first link
+   link_t *last;                  // last link
+   link_t *current;               // current link.
+};
+
---
+++ branches/prog6/model.cpp
@@ -0,0 +1,118 @@
+/**********************************************************************
+ * Kevin Matthew Smith && Burns John Hudson
+ * kevin4 && burnsh
+ * CpSc 102 Section 003
+ * March 9, 2009
+ * Program 5
+ *
+ * Description
+ * This program parses and loads the data for the model structure.
+ * Also a dumper function.
+ ***********************************************************************/
+ 
+/* model.c */
+
+#include "ray.h"
+
+static char *items[] =
+{
+    "camera",
+    "material",
+    "plane",
+	"sphere",
+#if 0
+    "pplane",
+    "light",
+    "tiled_plane",
+    "ellipsoid",
+#endif
+};
+
+#define NUM_ITEMS (sizeof(items) / sizeof(char *))
+
+ void model_t::model_item_load(
+FILE    *in,
+char    *itemtype)
+{
+   int ndx;
+
+   ndx = table_lookup(items, NUM_ITEMS, itemtype);
+
+   assert(ndx >= 0);
+
+   switch (ndx)
+   {
+   case 0:
+      cam = new camera_t(in);
+      break;
+   case 1:
+      new material_t(in, this, 0);
+      break;
+   case 2:
+      new plane_t(in, this, 0);
+      break;
+   case 3:
+      new sphere_t(in, this, 0);
+      break;
+#if 0
+   case 4:
+      new light_t(in, this, 0);
+      break;
+#endif
+   }
+   return;
+}
+
+/* Load model data */
+
+ void model_t::model_load(
+FILE    *in)
+{
+   char itemtype[16];
+   int  count;
+
+   memset(itemtype, 0, sizeof(itemtype));
+
+/* Here itemtype should be one of "material",    */
+/* "light", "plane"                             */
+
+   count = fscanf(in, "%s", itemtype);
+   while (count == 1)
+   {
+      model_item_load(in, itemtype);
+      count = fscanf(in, "%s", itemtype);
+   }
+}
+
+/***/
+/* Init model data */
+
+model_t::model_t(
+FILE *in)
+{
+
+   mats = new list_t;
+   assert(mats != NULL);
+
+   lgts = new list_t;
+   assert(lgts != NULL);
+
+   objs = new list_t;
+   assert(objs != NULL);
+
+   model_load(in);
+
+}
+
+/***/
+/* dump model data */
+
+void model_t::dump(
+FILE          *out)
+{
+   cam->camera_dump(out);
+   material_dump(out, mats);
+   object_dump(out, objs);
+// light_dump(out, this);
+}
+
