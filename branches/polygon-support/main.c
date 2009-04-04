15bbb45781be2cb5d51d0b2ea413d3690522b6b8
kevin4@g.clemson.edu <kevin4@g.clemson.edu@da11cbfa-f01e-11dd-8c4e-4bbb75a408b7>
2009-04-04 17:10:14 +0000

    polygon-support branch added

---
+++ branches/polygon-support/main.c
@@ -0,0 +1,42 @@
+/**********************************************************************
+ * Kevin Matthew Smith && Burns John Hudson
+ * kevin4 && burnsh
+ * CpSc 102 Section 003
+ * March 25, 2009
+ * Program 6
+ *
+ * Description
+ * This is the main program that takes the command line arguments and 
+ * outputs an image to the requested ppm file.
+ ***********************************************************************/
+ 
+/* main.c */
+
+#include "ray.h"
+
+int main(
+int argc,
+char *argv[])
+{
+   model_t *model;
+
+/* Load and dump the model */
+
+   model = new model_t(stdin);
+   assert(model != NULL);
+   model->dump(stderr);
+
+#if 0
+   vec_t   raydir;
+   drgb_t  pix = {0, 0, 0};
+   cam_getdir(cam, 307, 83, &raydir);
+   ray_trace(model, &cam->view_point, &raydir, &pix, 0.0, NULL);
+#endif
+
+/* Raytrace the image */
+
+   image_create(model);
+
+   return(0);
+}
+
---
+++ branches/polygon-support/makefile
@@ -0,0 +1,27 @@
+RAYOBJS = main.o camera.o list.o  model.o material.o object.o plane.o \
+          parser.o  image.o raytrace.o sphere.o light.o
+#         illum.o tplane.o ellipse.o
+
+INCLUDE = ray.h rayhdrs.h rayfuns.h vector.h
+
+CFLAGS = -DAA_SAMPLES=1
+# -DDBG_PIX -DDBG_DRGB  -DAA_SAMPLES=4
+# -DSOFT_SHADOWS
+
+ray: $(RAYOBJS)
+	g++ -Wall -o ray -g  $(RAYOBJS) -lm
+
+$(RAYOBJS): $(INCLUDE) makefile
+
+.c.o: $<
+	-g++ -c -Wall $(CFLAGS) -c -g $<  2> $(@:.o=.err)
+	cat $*.err
+
+.cpp.o: $<
+	-g++ -c -Wall $(CFLAGS) -c -g $<  2> $(@:.o=.err)
+	cat $*.err
+
+clean:
+	rm *.o *.err
+
+
